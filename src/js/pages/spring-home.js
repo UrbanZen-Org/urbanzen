758d0293bec464f9c5634c1a450c06de60358864
Steven Fuentes <steveafuentes@gmail.com>
2018-04-26 16:20:20 -0400

    home slider

--- src/js/pages/spring-home.js
+++ src/js/pages/spring-home.js
@@ -1,17 +1,27 @@
 'use strict';
 
 var $ = require('jquery');
-var fullpage = require('fullpage.js');
+var imagesLoaded = require('imagesloaded');
+var slick = require('slick-carousel');
 
 var spring_home = {
   init: function(){
   },
 
   ready: function(){
-    $('#fullpage').fullpage({
-      scrollBar: true,
-      scrollOverflow:true
+    var slider = $('[data-slide-count]').filter(function() {
+      return $(this).attr('data-slide-count') > 1;
     });
+    if (slider.length){
+      var self = this;
+      $(slider).each(function(){
+        var $self = $(this);
+        imagesLoaded($(this), function(){
+          self.slideshow($self);
+        });
+
+      });
+    }
   },
   
   resize:function(){
@@ -19,6 +29,48 @@ var spring_home = {
   },  
   scroll: function(){
     
-  }
+  },
+  slideshow: function(slider){
+      $(slider).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      
+      var currentSlideClasses = slick.$slides[currentSlide].className;
+      if(currentSlideClasses.indexOf('video') !== -1){
+        var video = $(slick.$slides[currentSlide]).find('video')[0];
+        // video.pause();
+      }
+      var slideClasses = slick.$slides[nextSlide].className;
+      
+        if(slideClasses.indexOf('video') !== -1){
+          var video = $(slick.$slides[nextSlide]).find('video')[0];
+          if ( video.readyState === 4 ) {
+            $(slick.$slides[nextSlide]).addClass('loaded');
+            video.play();
+            video.currentTime = 0;
+          }
+        }
+      
+      });
+      $(slider).on('init', function(event,slick){
+        var slideClasses = slick.$slides[0].className;
+          if(slideClasses.indexOf('video') !== -1){
+            var video = $(slick.$slides[0]).find('video')[0];
+            
+              $(slick.$slides[0]).addClass('loaded');
+              video.play();         
+            
+          }
+      });
+
+      var options = {
+        prevArrow: '<div class="slick-arrow arrow-left"></div>',
+        nextArrow: '<div class="slick-arrow arrow-right"></div>',
+        fade: false,
+        arrows : true,
+        infinite: true,
+        slide: '.slide',
+        speed: 1000
+      };
+      $(slider).slick(options);
+    }
 };
 module.exports = spring_home;

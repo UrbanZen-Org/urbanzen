62399eaa14655910c5d6c5455c8a3ad1b1cfad53
Steven Fuentes <steveafuentes@gmail.com>
2018-05-15 11:56:24 -0400

    fade in furniture

--- src/js/pages/collection.js
+++ src/js/pages/collection.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var $ = require('jquery');
+var slick = require('slick-carousel');
+var imagesLoaded = require('imagesloaded');
 
 var tabletop = {
   init: function(){
@@ -8,21 +10,27 @@ var tabletop = {
 
   ready: function(){
   	if ($('.collection-slideshow').length){
-  		$('.collection-slideshow .collection-slides').slick({
-  			prevArrow: '<div class="slick-arrow arrow-left"></div>',
-	      nextArrow: '<div class="slick-arrow arrow-right"></div>',
-			  infinite: true,
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-  		});
+      imagesLoaded($('.collection-slideshow'), function(){
+    		$('.collection-slideshow .collection-slides').slick({
+    			prevArrow: '<div class="slick-arrow arrow-left"></div>',
+  	      nextArrow: '<div class="slick-arrow arrow-right"></div>',
+  			  infinite: true,
+  			  slidesToShow: 1,
+  			  slidesToScroll: 1
+    		});
+        $('.collection-slideshow').addClass('visible');
+      });
   	}
     if ($('.collection-mobile-slideshow').length){
-      $('.collection-mobile-slideshow .collection-mobile-slides').slick({
-        prevArrow: '<div class="slick-arrow arrow-left"></div>',
-        nextArrow: '<div class="slick-arrow arrow-right"></div>',
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
+      imagesLoaded($('.collection-mobile-slideshow'), function(){
+        $('.collection-mobile-slideshow .collection-mobile-slides').slick({
+          prevArrow: '<div class="slick-arrow arrow-left"></div>',
+          nextArrow: '<div class="slick-arrow arrow-right"></div>',
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        });
+        $('.collection-mobile-slideshow').addClass('visible');
       });
     }
   },

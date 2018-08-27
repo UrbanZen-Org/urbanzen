4f29590740692ccc448d7d3c4d9ef5dd94da0b8e
Steven Fuentes <steveafuentes@gmail.com>
2018-08-27 15:00:58 -0400

    sbl chnages

---
+++ wp-content/themes/uz2017/page-urban-zen-foundation.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	Template Name: Urban Zen Foundation
+*/
+	$templates = 'urban-zen-foundation.twig';
+	$context = Timber::get_context();
+	$context['post'] = new TimberPost();
+	
+	if (get_fields()){
+		foreach (get_fields() as $name=>$value){
+			$context['post']->$name = $value;
+		}
+	}
+
+	Timber::render($templates, $context);
